ea39738e7c141d745f2001454410d4667da396d2
katie-12 <katiedelrio12@gmail.com>
2024-10-11 21:40:07 +0000

    update primm_11.py finished code

--- primm_11.py
+++ primm_11.py
@@ -5,12 +5,19 @@ Name - Date
 """
 
 def main():
-  
-    num1: int = input("Enter a number: ")
-    num2: int = input("Enter another number: ")
-    total: int = num1+num2
+      #prompt users for numbers
+    num1: float = float(input("Enter a number: "))
+    num2: float = float(input("Enter another number: "))
+      #add numbers together
+    total: float = num1+num2
 
+      #output
     print(f"{num1} + {num2} = {total}")
-
+    print(f"{num1} - {num2} = {num1-num2}")
+    print(f"{num1} * {num2} = {num1*num2}")
+    print(f"{num1} ** {num2} = {num1**num2}")
+    print(f"{num1} / {num2} = {num1/num2}")
+    print(f"{num1} // {num2} = {num1//num2}")
+    print(f"{num1} % {num2} = {num1%num2}")
 if __name__ == "__main__":
   main()
